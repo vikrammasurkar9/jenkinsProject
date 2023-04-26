5ff5dbc7a9ab6c7bc36a0350286661814d55a2c8
vikram <vikram666.masurkar@gmail.com>
2023-04-26 11:31:50 +0530

    Project Added

---
+++ src/test/java/com/example/mySampleProject/MySampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mySampleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
